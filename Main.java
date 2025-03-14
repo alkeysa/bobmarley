e202c962de31bca70b15bc06f12dd4d4829ce3aa
Muhammad AlKindi <muhammadalkindih@gmail.com>
2025-03-14 22:58:32 +0700

    Initial Comit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String [] args){
+
+        // /n = escape sequence
+
+        System.out.print("I like Pizza \n");
+        System.out.println("It's really good!");
+        System.out.print("Buy me Pizza!");
+    }
+}
